9f35eb7e69ea43c75c366c5ebf1387fbd9cbd6bd
Ilya <dubovilya90@gmail.com>
2024-06-22 11:58:47 +0300

    Test request

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+const runtimeConfig  = useRuntimeConfig()
+const apiUrl: string = runtimeConfig.public.apiUrl
+
+const data = await useFetch(`${apiUrl}get-test`)
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <p>
+    Hello Nuxt {{ data }}!
+  </p>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL || 'https://example.com/api'
+    }
+  }
 })
